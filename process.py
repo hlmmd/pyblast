03f8e5cb5756df47e61638de65bdca697d085aa0
hlmmd <hlmmd1995@gmail.com>
2022-03-04 00:31:15 +0800

    update

---
+++ process.py
@@ -0,0 +1,19 @@
+import os
+if not os.path.exists('db'):
+    exit(0)
+os.system('rm -rf result/*')
+filepaths = []
+for path, dirs, files in os.walk('db'):
+    for file in files:
+        if file.endswith('.fasta'):
+            filepath = os.path.join(path, file)
+            filepath = filepath.replace('\\', '/')
+            filepaths.append(filepath)
+
+
+cmd_sequence = [ 0, 1]
+for t in cmd_sequence:
+    for filepath in filepaths:
+        cmd = 'python biopython.py {} {}'.format(filepath, t)
+        # print(cmd)
+        os.system(cmd)
--- start.sh
+++ start.sh
@@ -1,7 +1,23 @@
-rm -rf result/*.csv
+rm -rf result/*
+
+# python biopython.py db/China.fasta 0
+# python biopython.py db/China.fasta 1
+
+
+# python biopython.py db/Alpha.fasta 100
+# # python biopython.py db/B.1.427_B.1.429.fasta 100
+# python biopython.py db/BA.2.fasta 100
+# python biopython.py db/Beta.fasta 100
+# python biopython.py db/Delta.fasta 100
+# python biopython.py db/Gamma.fasta 100
+# python biopython.py db/Lambda.fasta 100
+# python biopython.py db/Mu.fasta 100
+# python biopython.py db/Omicron.fasta 100
+# python biopython.py db/Vum.fasta 100
 
 python biopython.py db/Alpha.fasta 0
-python biopython.py db/B.1.427_B.1.429.fasta 0
+#python biopython.py db/B.1.427_B.1.429.fasta 0
+python biopython.py db/Lambda.fasta 0
 python biopython.py db/BA.2.fasta 0
 python biopython.py db/Beta.fasta 0
 python biopython.py db/Delta.fasta 0
@@ -11,7 +27,8 @@ python biopython.py db/Omicron.fasta 0
 python biopython.py db/Vum.fasta 0
 
 python biopython.py db/Alpha.fasta 1
-python biopython.py db/B.1.427_B.1.429.fasta 1
+python biopython.py db/Lambda.fasta 1
+#python biopython.py db/B.1.427_B.1.429.fasta 1
 python biopython.py db/BA.2.fasta 1
 python biopython.py db/Beta.fasta 1
 python biopython.py db/Delta.fasta 1
